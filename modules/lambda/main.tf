07a37652b9ac12b8977b99a8737d746ee41ea5dd
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-11-30 13:28:30 +0000

    feat: add IAM role creation for the Lambda function within the module

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,4 +1,26 @@
+locals {
+  role_name = "${var.function_name}_lambda_execution_role"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = local.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
 resource "aws_lambda_function" "lambda_function" {
   function_name = var.function_name
-  role          = var.role
+  role          = aws_iam_role.lambda_execution_role.arn
 }
