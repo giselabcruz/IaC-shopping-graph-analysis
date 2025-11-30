7d678642069040cac1ec7d4567a37608fbaa8d64
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-11-30 13:21:45 +0000

    feat: Add configurable IAM role for the Lambda function

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,3 +1,4 @@
 resource "aws_lambda_function" "lambda_function" {
   function_name = var.function_name
+  role          = var.role
 }
--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -2,3 +2,9 @@ variable "function_name" {
   type        = string
   description = "Name of the lambda function"
 }
+
+variable "role" {
+  type        = string
+  description = "Role ARN for the lambda function"
+}
+
